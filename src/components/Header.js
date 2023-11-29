466f5cd57587dae66888e9880c979b722716539c
aniket239 <aniketkrbiwas239@gmail.com>
2023-11-29 18:56:04 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,17 +12,37 @@ function Header() {
       <div className="logo">
         {/* Logo content */}
       </div>
-      
+
       <div className="menu-icon" onClick={toggleNav}>
         {isMobileNavVisible ? <span>✖</span> : <span>☰</span>} {/* Toggle between close and hamburger icon */}
       </div>
 
       <nav className="desktop-nav">
-        {/* Desktop navigation items */}
         <ul>
-          <li><a href="https://www.google.com">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <a class="fancy" href="https://google.com">
+              <span class="top-key"></span>
+              <span class="text">About</span>
+              <span class="bottom-key-1"></span>
+              <span class="bottom-key-2"></span>
+            </a>
+          </li>
+          <li>
+            <a class="fancy" href="https://google.com">
+              <span class="top-key"></span>
+              <span class="text">Projects</span>
+              <span class="bottom-key-1"></span>
+              <span class="bottom-key-2"></span>
+            </a>
+          </li>
+          <li>
+            <a class="fancy" href="https://google.com">
+              <span class="top-key"></span>
+              <span class="text">Contact</span>
+              <span class="bottom-key-1"></span>
+              <span class="bottom-key-2"></span>
+            </a>
+          </li>
         </ul>
       </nav>
 
