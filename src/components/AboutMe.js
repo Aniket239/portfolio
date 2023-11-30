cf62b1829da6fb730a4867e0e9d7d9e8f6940266
aniket239 <aniketkrbiwas239@gmail.com>
2023-11-30 11:54:43 +0530

    commit

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 function AboutMe() {
+  const whatsappNumber = "6290034904"; // Your WhatsApp number
+  const whatsappLink = `https://wa.me/${whatsappNumber}`;
+
   return (
     <section id="about" className="about-me-section">
       <div className="about-me-content">
-        <h1 className="typing-effect">Hi, I am a <span className="highlight">Designer</span> & <span className="highlight">Webflow Expert</span></h1>
-        <h2 className="typing-effect">Freelancer Expert in Webflow Development</h2>
-        <p>1+ years of experience in Webflow development. My mission is to design and develop a website that you and your audience love.</p>
-        <button className="whatsapp-btn">Let's Chat on WhatsApp</button>
+        <div className='introduction'>
+          <h1>Hi, I am a </h1><h1 className="typing-effect">Web Developer</h1>
+        </div>
+        <div className="ball"></div>
+        <h2>Freelancer Expert in Web Development</h2>
+        <p>1+ years of experience in Web development. My mission is to design and develop a website that you and your audience love.</p>
+        <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp-btn">Let's Chat on WhatsApp</a>
       </div>
       <div className="about-me-image">
         {/* Include your image here */}
