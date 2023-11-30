b40fdb60074b0519bd8b36c0b4efc3bcb31b01b4
aniket239 <aniketkrbiwas239@gmail.com>
2023-11-30 14:47:16 +0530

    commit

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
         <div className='introduction'>
           <h1>Hi, I am a </h1><h1 className="typing-effect">Web Developer</h1>
         </div>
-        <div className="ball"></div>
+        <div className="ball" hidden></div>
         <h2>Freelancer Expert in Web Development</h2>
         <p>1+ years of experience in Web development. My mission is to design and develop a website that you and your audience love.</p>
         <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp-btn">Let's Chat on WhatsApp</a>
