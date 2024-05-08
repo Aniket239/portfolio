b502d4637a2a11533fcd646ed838f793c8f3124b
Aniket239 <aniketbiswas2392001@gmail.com>
2024-05-08 17:37:48 +0530

    header

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,15 +3,13 @@ import './About.css'
 const About = () => {
   return (
     <section id="about">
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <h2>About section</h2>
       <div>
-        
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
       
     </section>
