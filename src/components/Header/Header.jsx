45a42c5a6d0cf2acaf4d42168b92b313ffafb24c
Aniket239 <aniketbiswas2392001@gmail.com>
2024-05-09 14:46:50 +0530

    header responsivw

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,75 +1,89 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import {Link } from "react-router-dom";
 import './Header.css'
 
-const Header = () =>{
+const theme = ()=>{
   function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
- function menu(){
-  var nav_items = document.getElementById("nav-container");
-  if (nav_items.style.display === "block") {
-    nav_items.style.display = "none";
-  } else {
-    nav_items.style.display = "block";
   }
- }
+  return(
+    <div className="container">
+    <label className="toggle" htmlFor="switch">
+        <input id="switch" className="input" type="checkbox" onChange={myFunction}  />
+        <div className="icon icon--moon">
+            <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path clipRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" fillRule="evenodd"></path>
+            </svg>
+        </div>
+        <div className="icon icon--sun">
+            <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
+            </svg>
+        </div>
+    </label>
+  </div>
+  );
+}
+const Header = () =>{
 
- const nav_item = () => {
-  var nav_items = document.getElementById("nav-container");
-  var menu = document.getElementById("menu").checked;
-  if (menu === true)
-    {
-      if (nav_items.style.display === "block") {
-        nav_items.style.display = "none";
-        document.getElementById("menu").checked=false
-      }
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width to the innerWidth of the browser window
+      setWindowWidth(window.innerWidth);
     }
- }
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  function menu(){
+    var nav_items = document.getElementById("nav-container");
+    if (nav_items.style.display === "block") {
+      nav_items.style.display = "none";
+    } else {
+      nav_items.style.display = "block";
+    }
+  }
+
+  const nav_item = () => {
+    var nav_items = document.getElementById("nav-container");
+    var menu = document.getElementById("menu").checked;
+    if (menu === true)
+      {
+        if (nav_items.style.display === "block") {
+          nav_items.style.display = "none";
+          document.getElementById("menu").checked=false
+        }
+      }
+  }
+
   return (
     <div className="header">
     <header className="header-desktop">
       <Link to="/portfolio">About</Link>
       <Link to="/projects">Projects</Link>
       <Link to="/contacts">Contact</Link>
-      <div class="container">
-        <label class="toggle" for="switch">
-            <input id="switch" class="input" type="checkbox" onChange={myFunction}  />
-            <div class="icon icon--moon">
-                <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" fill-rule="evenodd"></path>
-                </svg>
-            </div>
-            <div class="icon icon--sun">
-                <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
-                </svg>
-            </div>
-        </label>
-      </div>
+      {windowWidth  > 767 ? theme() : null}
     </header>
+    
     <header className="header-mobile">
-      <div class="container">
-        <label class="toggle" for="switch">
-            <input id="switch" class="input" type="checkbox" onChange={myFunction}  />
-            <div class="icon icon--moon">
-                <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" fill-rule="evenodd"></path>
-                </svg>
-            </div>
-            <div class="icon icon--sun">
-                <svg height="32" width="32" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
-                </svg>
-            </div>
-        </label>
-      </div>
-      <label class="hamburger">
+      {theme()}
+      <label className="hamburger">
         <input type="checkbox" id="menu" onChange={menu}/>
           <svg viewBox="0 0 32 32">
-            <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
-            <path class="line" d="M7 16 27 16"></path>
+            <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+            <path className="line" d="M7 16 27 16"></path>
           </svg>
       </label>
     </header>
