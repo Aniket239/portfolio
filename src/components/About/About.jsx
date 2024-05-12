5f378f927511c806ee7e9856e72f821c92f8d9c9
Aniket239 <aniketbiswas2392001@gmail.com>
2024-05-12 16:14:04 +0530

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,14 +7,16 @@ const About = () => {
         <div className="about_container_about">
           <h1>About Me</h1>
         </div>
-        <div className="about_items">
-          <div className="portfolio">
-            <h1>PORTFOLIO</h1>
+        <div>
+          <div className="about_items">
+          <div className="about_item1">
+            <h1>Portfolio</h1>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+            <div className="contact">Contact</div>
+            <div className="projects">Projects</div>
+            <div className="resume">resume</div>
+            <div className="about_item5">right</div>
+          </div>
         </div>
       </div>
     </section>
